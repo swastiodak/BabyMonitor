2fbe9677e2acec1e1149c763e1f35b72ee38768f
swastiodak <82050829+swastiodak@users.noreply.github.com>
2022-07-27 18:40:26 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+function setup() {
+    canvas = createCanvas(500, 500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(500, 500);
+    video.hide();
+}
+
+status = "";
+object = [];
+function preload() {
+    sound = loadSound("siren.mp3")
+ }
+
+function draw() {
+    image(video, 0, 0, 500, 500);
+    if (status != "") {
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        objectDetector.detect(video, gotResult);
+        for (index = 0; index < object.length; index++) {
+            document.getElementById("status").innerHTML = "Status : Objects Detected";
+            document.getElementById("objNo").innerHTML = "Number of Objects Detected : " + object.length;
+            percent = floor(object[index].confidence * 100);
+            fill(r, g, b);
+            text(object[index].label + " " + percent + "%", object[index].x + 10, object[index].y + 20);
+            noFill();
+            stroke(r, g, b);
+            rect(object[index].x, object[index].y, object[index].width, object[index].height);
+        }
+    }
+}
+
+function modelLoaded() {
+    console.log('model is loaded');
+    status = true;
+}
+
+function gotResult(error, result) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log(result);
+        object = result;
+        if(result[0].label == "baby"){
+            siren.play();
+        }
+    }
+}
+
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById('status').innerHTML = "Status: Detecting objects";
+}
